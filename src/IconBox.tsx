d38e4da6236236ae136add2f144939004d6a53dd
corourke <cameron.orourke@gmail.com>
2021-09-23 12:05:05 -0700

    Updating icon notes

--- src/IconBox.tsx
+++ src/IconBox.tsx
@@ -6,12 +6,13 @@ interface IconProps {
 }
 
 // I was using https://cryptoicons.org/ which is versatile, but it doesn't have all the coins
-// See: https://github.com/TokenTax/cryptoicon-api
-// Alternate: https://github.com/ErikThiart/cryptocurrency-icons
-// Alternate: https://github.com/reddavis/Crypto-Icons-API/tree/master/public/svg/icon
+// See alternate: https://github.com/TokenTax/cryptoicon-api
+// It might be better to have all the SVG files in the project:
+//   https://github.com/ErikThiart/cryptocurrency-icons
+//   https://github.com/reddavis/Crypto-Icons-API/tree/master/public/svg/icon
 
-// TODO: Need to try fetching icon data and handle errors
-// TODO: Might be better to have all the SVG files in the project
+// TODO: Need to handle errors and provide a fallback
+// TODO: Set the background color to a tint of the main icon color
 
 export const IconBox: React.FC<IconProps> = ({ symbol }) => {
   return (
